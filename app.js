6a53d3d57e1476cc44184d1d91543e808c97d365
noftaly <noftaly@gmail.com>
2019-12-30 18:43:31 +0100

    :bug: Adding back the user object to the page context

--- app.js
+++ app.js
@@ -91,6 +91,8 @@ app.use((req, res, next) => {
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
   res.locals.error = req.flash('error');
+  // Setting user
+  res.locals.user = req.user;
   return next();
 });
 
