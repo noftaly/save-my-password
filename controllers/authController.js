46260b8e79b535fb46e3e646ae48fed8e67b91b8
noftaly <noftaly@gmail.com>
2020-03-15 22:04:32 +0100

    :art: Refactor account validation in the register route

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,7 +9,7 @@ export function getRegister(_req, res) {
 }
 
 // Register
-export function postRegister(req, res) {
+export async function postRegister(req, res) {
   const { name, firstname, username, email, password, passwordConfirm } = req.body;
   const errors = [];
 
@@ -19,27 +19,26 @@ export function postRegister(req, res) {
   if (password.length <= 6)
     errors.push({ msg: 'Le mot de passe doit faire au moins 6 caractères.' });
 
+  const existingEmail = await User.findOne({ email });
+  if (existingEmail)
+    errors.push({ msg: 'Cet email est déjà associé à un compte.' });
+
+  const existingUsername = await User.findOne({ username });
+  if (existingUsername)
+    errors.push({ msg: "Ce nom d'utilisateur est déjà utilisé." });
+
   if (errors.length > 0) {
     res.locals = { errors, name, firstname, username, email };
     return res.status(400).render('register', { title: 'Inscription', page: 'register' });
   }
 
-  User.findOne({ email }).then(async (existingUser) => {
-    if (existingUser) {
-      errors.push({ msg: 'Email already exists' });
-      res.locals = { errors, name, firstname, username, email };
-      return res.status(400).render('register', { title: 'Inscription', page: 'register' });
-    }
+  const newUser = new User({ name, firstname, username, email, password });
+  const hash = await bcrypt.hash(password, 10).catch(console.error);
+  newUser.password = hash;
+  await newUser.save().catch(console.error);
 
-    const newUser = new User({ name, firstname, username, email, password });
-    const hash = await bcrypt.hash(password, 10).catch(console.error);
-    newUser.password = hash;
-    newUser.save()
-      .then(() => {
-        req.flash('success_msg', 'Vous êtes bien enregistré. Veuillez maintenant vous connecter.');
-        res.redirect(301, '/login');
-      }).catch(console.error);
-  });
+  req.flash('success_msg', `Bienvenue ${username} ! Vous êtes bien enregistré. Veuillez maintenant vous connecter.`);
+  res.redirect(301, '/login');
 }
 
 // Login Page
