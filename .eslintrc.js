a1eb736e0101062be34e5d40d005018b6621a441
noftaly <noftaly@gmail.com>
2020-03-15 21:47:53 +0100

    :arrow_up: Update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
   extends: 'noftalint',
   rules: {
     'capitalized-comments': 'off',
+    // For some reason it cannot find this rule, so we disable it for now
+    'unicorn/prefer-replace-all': 'off',
   },
   env: {
     browser: true,
