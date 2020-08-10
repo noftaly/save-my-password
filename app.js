063336d5b2f121a421997a8a4c4425f817d2ee60
Elliot Maisl <elliot.maisl@gmail.com>
2020-08-10 19:47:22 +0200

    🚨 Fix lint error

--- app.js
+++ app.js
@@ -50,9 +50,8 @@ mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on('error', (err) => {
-  console.error(err);
   console.log('MongoDB connection error. Please make sure MongoDB is running.');
-  process.exit();
+  throw err;
 });
 
 // Setting up Handlebars
